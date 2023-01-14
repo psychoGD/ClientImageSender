69985c7536a9dbf5edb4e61c80f97458a0354f37
Huseyn Abbasov <nelbekistekan12@gmail.com>
2023-01-14 22:35:40 +0400

    Add project files.

---
+++ ClientImageSender/MainViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Drawing;
+//using System.Windows.Controls;
+
+namespace ClientImageSender
+{
+    public class MainViewModel : BaseViewModel
+    {
+        //public RelayCommand DropEvent { get; set; }
+
+
+        public RelayCommand ClickCommand { get; set; }
+
+        private string imageSource;
+
+        public string ImageSource
+        {
+            get { return imageSource; }
+            set { imageSource = value; OnPropertyChanged(); }
+        }
+
+        public BitmapImage Image { get; set; }
+
+        public bool isConnected { get; set; } = true;
+
+
+        public MainViewModel()
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var ipAddress = IPAddress.Parse("192.168.56.1");
+            var port = 80;
+            var ep = new IPEndPoint(ipAddress, port);
+
+            try
+            {
+                socket.Connect(ep);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Cant Connect To Server");
+                isConnected = false;
+            }
+            ClickCommand = new RelayCommand(o =>
+            {
+                if (isConnected)
+                {
+                    if (socket.Connected)
+                    {
+                        byte[] bytes = (byte[])(new ImageConverter()).ConvertTo(Image, typeof(byte[]));
+                        socket.Send(bytes);
+                    }
+                }
+
+
+            });
+        }
+
+
+    }
+}
---
+++ ClientImageSender/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientImageSender
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainViewModel mainViewModel { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            mainViewModel= new MainViewModel();
+            this.DataContext= mainViewModel;
+        }
+
+        private void Window_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetData(DataFormats.FileDrop) != null)
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                BitmapImage myBitmapImage = new BitmapImage(new Uri($@"{files[0]}", UriKind.Relative));
+                myBitmapImage.CacheOption = BitmapCacheOption.Default;
+                mainViewModel.Image= myBitmapImage;
+                mainViewModel.ImageSource = myBitmapImage.UriSource.ToString();
+
+            }
+        }
+    }
+}
